0f0d6f33db31d5340c5f2b1af994fe9713af82b6
nickahickman <nickahickman@gmail.com>
2020-09-10 20:07:13 -0400

    initial commit

---
+++ deliverable2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string headsOrTailsGuess = "";
+            float numberOfFlips = 0;
+            float correctCount = 0;
+            Random rand = new Random();
+
+            Console.Write("Guess which will have more: heads or tails? ");
+            headsOrTailsGuess = Console.ReadLine().ToLower();
+
+            Console.Write("How many flips? ");
+            numberOfFlips = float.Parse(Console.ReadLine());
+
+            runCoinFlips(numberOfFlips);
+
+            void runCoinFlips(float numOfFlips) {
+                for (int count = 0; count < numOfFlips; count++)
+                {
+                    string flipResult = flipCoin();
+                    if (flipResult == headsOrTailsGuess) {
+                        correctCount++;
+                    }
+                }
+            }
+
+            string flipCoin() {
+                float flip = rand.Next(0, 2);
+                string result = "";
+
+                if (flip % 2 == 0) {
+                    result = "heads";
+                }
+                else {
+                    result = "tails";
+                }
+
+                Console.WriteLine(result);
+                return result;
+            }
+
+            float calculateGuessAccuracy(float correctGueses, float chances) {
+                return correctGueses / chances * 100;
+            }
+
+            Console.WriteLine($"Your guess, {headsOrTailsGuess}, came up {correctCount} time(s)");
+            Console.WriteLine($"That's {calculateGuessAccuracy(correctCount, numberOfFlips):0.0}%");
+        }
+    }
+}
